4d2ab8b2f8071a37776ff9468d1a6a8ac04472b0
volderpayoda <volderpayoda@gmail.com>
2018-02-28 19:40:20 -0300

    Prueba de integracion con SQL

--- datos.js
+++ datos.js
@@ -1,10 +1,49 @@
 var seq = 0; //sacar
 var ordenanzas = []; //reemplazar con archivo de bd
 
+var pg = require('pg');
+
+var connString = "postgres://postgres:volderpayoda96@localhost:5432/legal"
+
+function getPgClient(callback) {
+    pg.connect(connString, function (err, client, done) {
+        if (err) {
+            done(true);
+            callback(err);
+            return;
+        }
+        callback(null, client, done);
+    });
+}
+
+function queryPg(query, callback) {
+    getPgClient(function (err, client, done) {
+        if (err) {
+            callback(err);
+            return;
+        }
+        client.query(query, function (err, results) {
+            if (err) {
+                done(true);
+                callback(err);
+                return;
+            }
+            done();
+            callback(null, results);
+
+        });
+    });
+};
 module.exports = {
     ordenanzas: {
         selectAll: function (callback) {
-            callback(null, ordenanzas);
+            queryPg("select * from ordenanzas", function(err, resultś) {
+                if (err) {
+                    callback(err);
+                    return;
+                }
+                callback(null, results.rows);
+            })
         },
         insert: function (ordenanza, callback) {
             var _id = "ID" + seq++;
