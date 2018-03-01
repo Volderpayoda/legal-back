9db265450e286d817543d34f3011c0d56fbc9f77
volderpayoda <volderpayoda@gmail.com>
2018-03-01 06:57:55 -0500

    Arreglando conexion con BD

--- datos.js
+++ datos.js
@@ -1,49 +1,20 @@
 var seq = 0; //sacar
 var ordenanzas = []; //reemplazar con archivo de bd
 
-var pg = require('pg');
+var { Pool } = require('pg');
+var pool = new Pool();
 
-var connString = "postgres://postgres:volderpayoda96@localhost:5432/legal"
 
-function getPgClient(callback) {
-    pg.connect(connString, function (err, client, done) {
-        if (err) {
-            done(true);
-            callback(err);
-            return;
-        }
-        callback(null, client, done);
-    });
-}
-
-function queryPg(query, callback) {
-    getPgClient(function (err, client, done) {
-        if (err) {
-            callback(err);
-            return;
-        }
-        client.query(query, function (err, results) {
-            if (err) {
-                done(true);
-                callback(err);
-                return;
-            }
-            done();
-            callback(null, results);
-
-        });
-    });
-};
 module.exports = {
     ordenanzas: {
         selectAll: function (callback) {
-            queryPg("select * from ordenanzas", function(err, results) {
-                if (err) {
-                    callback(err);
-                    return;
-                }
-                callback(null, results.rows);
-            })
+           pool.query("select * from ordenanzas", function(err, res) {
+             if (err) {
+               callback(err);
+               return
+             }
+             callback(null, results.rows);
+           }); 
         },
         insert: function (ordenanza, callback) {
             var _id = "ID" + seq++;
