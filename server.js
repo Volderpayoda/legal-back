a6a777f455f67aa1055837d2b79caa2ea4d584f9
volderpayoda <volderpayoda@gmail.com>
2018-03-02 09:26:25 -0500

    Raios

--- server.js
+++ server.js
@@ -12,6 +12,6 @@ app.use("/api/ordenanzas", ordenanzas);
 
 app.use(express.static(path.join(__dirname, "public")));
 
-app.listen(80, function () {
+app.listen(8080, function () {
     console.log("Servidor iniciado");
 });
