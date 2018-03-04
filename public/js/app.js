4fe4b75ce53886706672d62c9bb527a7b38aca23
Volderpayoda <volderpayoda@gmail.com>
2018-03-04 20:08:58 -0300

    qwerwqr

--- public/js/app.js
+++ public/js/app.js
@@ -35,10 +35,10 @@ app.controller("ordenanzas-ctrl", function($scope, $http) {
 });
 
 app.controller("altaOrdenanzasCtrl", function($scope, $http) {
-    $scope.ordenanza.nroOrdenanza=null;
-    $scope.ordenanza.tema=null;
+    $scope.nroOrdenanza=null;
+    $scope.tema=null;
     
-    $scope.postdata = function(ordenanza) {
+    $scope.postdata = function(nroOrdenanza,tema) {
     var data = {"nroOrdenanza": ordenanza.nroOrdenanza, "tema": ordenanza.tema}
     $http.post("http://volderpayoda.sytes.net/api/ordenanzas", JSON.stringify(data)).then(function(response) {
         if (response.data) $scope.msg = "exito";
