ae3a8150db40b913eea3c2c45f1bb5a2efffa942
Volderpayoda <volderpayoda@gmail.com>
2018-03-04 21:04:24 -0300

    dsada
    asddsadas

--- db/index.js
+++ db/index.js
@@ -3,9 +3,9 @@ const { Pool } = require('pg')
 const pool = new Pool()
 
 module.exports = {
-  query: (text, params, callback) => {
+  query: function(text, params, callback) {
     const start = Date.now()
-    return pool.query(text, params, (err, res) => {
+    return pool.query(text, params, function(err, res) {
       const duration = Date.now() - start
       console.log('executed query', { text, duration, rows: res.rowCount })
       callback(err, res)
--- routes/ordenanzas.js
+++ routes/ordenanzas.js
@@ -17,18 +17,16 @@ router.post("/", function (req, res, next) {;
     var ordenanza = req.body;
     var text = "insert into ordenanzas(nroOrdenanza, tema, promulgacion, fechaPromulgacion, nroPromulgacion, observacion, nroActSimple, presento, origen, reglamentada) "
     + "values ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)";
-    var params = [ordenanza.nroOrdenanza, ordenanza.tema, ordenanza.promulgacion, ordenanza.fechaPromulgacion, ordenanza.nroPromulgacion, ordenanza.observacion, ordenanza.nroActSimple, ordenanza.presento, ordenanza.origen, ordenanza.reglamentada]
-    console.log(ordenanza);
-    res.json(req.body);
-    //db.query(text, params, function(err, results){
-    //    if (err) {
-    //        next(err);
-    //        return;
-    //    }
-    //    res.status(201).json({
-    //    results: results
-    //    })
-  //  })
+    var params = [ordenanza.nroOrdenanza, ordenanza.tema, ordenanza.promulgacion, ordenanza.fechaPromulgacion, ordenanza.nroPromulgacion, ordenanza.observacion, ordenanza.nroActSimple, ordenanza.presento, ordenanza.origen, ordenanza.reglamentada];
+    db.query(text, params, function(err, results){
+        if (err) {
+            next(err);
+            return;
+        }
+        res.status(201).json({
+        results: results
+        })
+    })
 });
 
 router.get("/:id", function (req, res, next) {
