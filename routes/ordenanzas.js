95959a8e26f05d9c75e139af4ba9ae0dd9ba479f
Volderpayoda <volderpayoda@gmail.com>
2018-03-04 23:25:59 -0300

    dsa

--- routes/ordenanzas.js
+++ routes/ordenanzas.js
@@ -14,36 +14,48 @@ router.get("/", function(req, res, next){
 })
 
 router.post("/", function (req, res, next) {;
-    var ordenanza = req.body;
-    var text = 'insert into ordenanzas("nroOrdenanza", "tema", "promulgacion", "fechaPromulgacion", "nroPromulgacion", "observacion", "nroActSimple", "presento", "origen", "reglamentada") '
-    + "values ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)";
-    var params = [ordenanza.nroOrdenanza, ordenanza.tema, ordenanza.promulgacion, ordenanza.fechaPromulgacion, ordenanza.nroPromulgacion, ordenanza.observacion, ordenanza.nroActSimple, ordenanza.presento, ordenanza.origen, ordenanza.reglamentada];
-    db.query(text, params, function(err, results){
-        if (err) {
-            next(err);
-            return;
-        }
-        res.status(201).json({
-        results: results
-        })
-    })
+  var ordenanza = req.body;
+  var text = 'insert into ordenanzas("nroOrdenanza", "tema", "promulgacion", "fechaPromulgacion", "nroPromulgacion", "observacion", "nroActSimple", "presento", "origen", "reglamentada") '
+  + "values ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)";
+  var params = [ordenanza.nroOrdenanza, ordenanza.tema, ordenanza.promulgacion, ordenanza.fechaPromulgacion, ordenanza.nroPromulgacion, ordenanza.observacion, ordenanza.nroActSimple, ordenanza.presento, ordenanza.origen, ordenanza.reglamentada];
+  db.query(text, params, function(err, results){
+      if (err) {
+          next(err);
+          return;
+      }
+      res.status(201).json({
+      results: results
+      })
+  })
 });
 
 router.get("/:id", function (req, res, next) {
-    var _id = req.params.id;
-    var text = 'select * from ordenanzas where _id = $1';
-    var params = [_id];
-    db.query(text, params, function(err, results){
-      if (err) {
-        next(err);
-        return;
-      }
-      console.log(results);
-      res.json(results.rows[0]);  
-    })
+  var _id = req.params.id;
+  var text = 'select * from ordenanzas where _id = $1';
+  var params = [_id];
+  db.query(text, params, function(err, results){
+    if (err) {
+      next(err);
+      return;
+    }
+    console.log(results);
+    res.json(results.rows[0]);  
+  })
 });
 
-// TODO: delete
+router.delete("/:id", function (req, res, next) {
+  var _id = req.params.id;
+  var text = 'delete from ordenanzas where _id = $1';
+  var params = [_id];
+  db.query(text, params, function(err, results){
+    if (err){
+      next(err);
+      return;
+    }
+    console.log(results);
+    res.json(results);
+  })
+})
 
 // TODO: update
 
