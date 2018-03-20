5df81ebe54e2563fd711dad454566a46e120aa57
Volderpayoda <volderpayoda@gmail.com>
2018-03-20 19:09:20 -0300

    Mejorando la estructura de la aplicación
    O al menos eso creo

--- routes/user.js
+++ routes/user.js
@@ -6,3 +6,5 @@ var router = express.Router();
 router.get('/', function(req, res){
     res.render('index', {user: req.user})
 })
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,14 +1,31 @@
 // dependencies
 var express = require("express");
 var bodyParser = require("body-parser");
+var cookieParser = require('cookie-parser');
 var path = require("path");
 var mongoose = require('mongoose');
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
 
 var ordenanzas = require("./routes/ordenanzas");
+var usuarios = require("./routes/user")
 
 var app = express();
+
+// mongoose
+mongoose.connect(MONGOURL);
+
+// app config
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(require('express-session')({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 // passport config
 var Account = require('./models/account');
@@ -16,13 +33,11 @@ passport.use(new LocalStrategy(Account.authenticate()));
 passport.serializeUser(Account.serializeUser());
 passport.deserializeUser(Account.deserializeUser());
 
-// mongoose
-mongoose.connect(MONGOURL);
-
-app.use(bodyParser.json());
-
+// route mounting
 app.use("/api/ordenanzas", ordenanzas);
+app.use("/api/usuarios", usuarios);
 
+// serving Angular app
 app.use(express.static(path.join(__dirname, "public")));
 
 app.listen(80, function () {
