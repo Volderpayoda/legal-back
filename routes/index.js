654026a0ae5fe5c50046a77b0e3526568db4a0b8
Volderpayoda <volderpayoda@gmail.com>
2018-03-20 16:59:34 -0300

    DAS

---
+++ routes/index.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var passport = require('passport');
+var Account = require('../models/account');
+var router = express.Router();
+
+router.get('/', function(req, res){
+    res.render('index', {user: req.user})
+})
--- server.js
+++ server.js
@@ -1,10 +1,23 @@
+// dependencies
 var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
+var mongoose = require('mongoose');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
 
 var ordenanzas = require("./routes/ordenanzas");
 
 var app = express();
+
+// passport config
+var Account = require('./models/account');
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
+
+// mongoose
+mongoose.connect(MONGOURL);
 
 app.use(bodyParser.json());
 
