ab6acdcb1d52dafa28f449866bcfc3a2b1d4c8c9
Volderpayoda <volderpayoda@gmail.com>
2018-03-21 14:40:13 -0300

    agregando control de sesion

--- routes/user.js
+++ routes/user.js
@@ -3,11 +3,10 @@ var passport = require('passport');
 var Account = require('../models/account');
 var router = express.Router();
 
-router.post('/register', function(req, res, next){
+router.post('/register', function(req, res){
     Account.register(new Account({ username: req.body.username}), req.body.password, function(err, account) {
         if (err) {
-            next(err);
-            return;
+            return res.sendStatus(500);
         }
         passport.authenticate('local')(req, res, function(){
             res.sendStatus(201);
@@ -15,8 +14,31 @@ router.post('/register', function(req, res, next){
     })
 })
 
-router.post('/login', passport.authenticate('local'), function(req, res){
-    res.sendStatus(200);
+router.post('/login', function(req, res, next){
+    passport.authenticate('local', function(err, user, info) {
+        if (err) {
+            return next(err);
+        }
+        if (!user) {
+            return res.status(401).json({
+                err: info
+            });
+        }
+        req.logIn(user, function(err){
+            if (err) {
+                return res.status(500).json({
+                    err: 'No se pudo iniciar sesión'
+                });
+            }
+            res.status(200).json({
+                status: 'Inicio de sesión correcto'
+            });
+        }); 
+    })(req, res, next);
 })
 
+router.get('/logout', function(req, res){
+    req.logout();
+    res.sendStatus(200);
+})
 module.exports = router;
