3e87abf4bfacb5bae0327ea159cf5ae782ae20dc
Volderpayoda <volderpayoda@gmail.com>
2018-03-24 14:40:03 -0300

    intento logout

--- public/js/app.js
+++ public/js/app.js
@@ -38,11 +38,11 @@ $stateProvider
 ;
     $urlRouterProvider.otherwise('/home');
 });
-
+/*
 app.controller('logout-ctrl', function($scope, $state, AuthService){
     $scope.logout = function(){
         AuthService.logout().then(function(){
             $state.go('login');
         });
     };
-});
+});*/
--- public/js/logout-ctrl.js
+++ public/js/logout-ctrl.js
@@ -1,4 +1,4 @@
-app.controller('logout-ctrl', function($scope, $state, AuthService){
+app.controller("logout-ctrl", function($scope, $state, AuthService){
     $scope.logout = function(){
         AuthService.logout().then(function(){
             $state.go('login');
