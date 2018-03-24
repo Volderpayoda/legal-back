70bfd27a773c72be61baa495d6a4868bf6cfd915
Volderpayoda <volderpayoda@gmail.com>
2018-03-24 14:37:47 -0300

    adsf

--- public/js/app.js
+++ public/js/app.js
@@ -39,3 +39,10 @@ $stateProvider
     $urlRouterProvider.otherwise('/home');
 });
 
+app.controller('logout-ctrl', function($scope, $state, AuthService){
+    $scope.logout = function(){
+        AuthService.logout().then(function(){
+            $state.go('login');
+        });
+    };
+});
