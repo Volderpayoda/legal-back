f2e060b01f9632f4f06b2e59e6630b2493f43b09
Volderpayoda <volderpayoda@gmail.com>
2018-03-24 15:08:16 -0300

    probando el run

--- public/js/app.js
+++ public/js/app.js
@@ -6,41 +6,41 @@ $stateProvider
         url:'/login',
         templateUrl: 'templates/login.html',
         controller: 'login-ctrl',
-        access: {restricted: false}
+        //access: {restricted: false}
         })
     .state('logout', {
         url:'/logout',
         controller: 'logout-ctrl',
-        access: {restricted: true}
+        //access: {restricted: true}
     })
     .state('home', {
         url: '/home',
         templateUrl: 'templates/home.html',
         controller:'logout-ctrl',
-        access: {restricted: true}
+        //access: {restricted: true}
         })
     .state('ordenanzas', {
         url: '/ordenanzas',
         templateUrl: 'templates/ordenanzas.html',
         controller:"ordenanzas-ctrl",
-        access: {restricted: true}      
+        //access: {restricted: true}      
         })
     .state('submit', {
         url: '/submit',
         templateUrl: 'templates/submit.html',
-        access: {restricted: true}
+        //access: {restricted: true}
         })
     .state('alta-ordenanzas', {
         url: '/alta-ordenanzas',
         templateUrl: 'templates/alta-ordenanzas.html',
         controller:"altaOrdenanzasCtrl",
-        access: {restricted: true}
+        //access: {restricted: true}
         })
     .state('modif-ordenanzas', {
         url: '/modif-ordenanzas',
         templateUrl: 'templates/modif-ordenanzas.html',
         controller:"modifOrdenanzasCtrl",
-        access: {restricted: true}           
+        //access: {restricted: true}           
         })
 ;
     $urlRouterProvider.otherwise('/login');
@@ -49,7 +49,7 @@ $stateProvider
 app.run(function($rootScope, $state, AuthService){
     $rootScope.$on('$stateChangeStart',
     function(event, next, current){
-        if (next.access.restricted && AuthService.isLoggedIn() === false) {
+        if (AuthService.isLoggedIn() === false) {
             $state.go('login');
         }
     }
