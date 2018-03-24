29c9185ad66c02f418ef392b2af662a6a83fc15f
Volderpayoda <volderpayoda@gmail.com>
2018-03-24 00:03:09 -0300

    asdf

--- public/js/login-ctrl.js
+++ public/js/login-ctrl.js
@@ -7,13 +7,12 @@ app.controller('login-ctrl', function ($scope, $state, AuthService) {
         $scope.disabled = true;
         // call login from service
         AuthService.login($scope.loginForm.username, $scope.loginForm.password).then(function () {
-            
             $scope.msg="aca";
             $state.go('home');
             $scope.disabled = false;
             $scope.loginForm = {};
         }), function () {
-            //$scope.error = true;
+            $scope.error = true;
             $scope.errorMessage = "Invalid username and/or password";
             $scope.disabled = false;
             $scope.loginForm = {};
