15ddb6330096d5536bad0d3f2c658a49e0acb83c
pabeli <limapaulabelen@gmail.com>
2018-04-10 20:12:25 -0300

    primer intento multiple views

--- public/js/app.js
+++ public/js/app.js
@@ -4,42 +4,92 @@ app.config(function($stateProvider, $urlRouterProvider){
 $stateProvider
     .state('login', {
         url:'/login',
-        templateUrl: 'templates/login.html',
-        controller: 'login-ctrl',
+        views:{
+            'nav':{
+
+            },
+            'content':{
+                templateUrl: 'templates/login.html',
+                controller: 'login-ctrl'},
+        },
         access: {restricted: false}
         })
+    
     .state('logout', {
         url:'/logout',
         controller: 'logout-ctrl',
         access: {restricted: true}
     })
+    
     .state('home', {
         url: '/home',
-        templateUrl: 'templates/home.html',
-        controller:'logout-ctrl',
+        views:{
+            'nav':{
+                templateUrl: 'templates/nav.html'
+            },
+            'content':{
+                templateUrl: 'templates/home.html',
+                controller:'logout-ctrl'
+            }
+        }, 
         access: {restricted: true}
         })
+
     .state('ordenanzas', {
         url: '/ordenanzas',
-        templateUrl: 'templates/ordenanzas.html',
-        controller:"ordenanzas-ctrl",
+        views:{
+            'nav':{
+                templateUrl:'templates/nav.html'
+            },
+            'content':{
+                templateUrl: 'templates/ordenanzas.html',
+                controller:"ordenanzas-ctrl",
+            },
+        },
         access: {restricted: true}      
         })
+
     .state('submit', {
         url: '/submit',
-        templateUrl: 'templates/submit.html',
+        views:{
+            'nav':
+            {
+                templateUrl:'templates/nav.html'
+            },
+            'content':{
+                templateUrl: 'templates/submit.html',
+            }
+        },
         access: {restricted: true}
         })
+
     .state('alta-ordenanzas', {
         url: '/alta-ordenanzas',
-        templateUrl: 'templates/alta-ordenanzas.html',
-        controller:"altaOrdenanzasCtrl",
+        views:{
+            'nav':
+            {
+                templateUrl:'templates/nav.html'
+            },
+            'content':{
+                templateUrl: 'templates/alta-ordenanzas.html',
+                controller:"altaOrdenanzasCtrl",
+            }
+        },
         access: {restricted: true}
         })
+
     .state('modif-ordenanzas', {
         url: '/modif-ordenanzas',
-        templateUrl: 'templates/modif-ordenanzas.html',
-        controller:"modifOrdenanzasCtrl",
+        views:{
+            'nav':
+            {
+                templateUrl:'templates/nav.html'
+            },
+            'content':{
+                templateUrl: 'templates/modif-ordenanzas.html',
+                controller:"modifOrdenanzasCtrl",
+            }
+        },
         access: {restricted: true}           
         })
 ;
