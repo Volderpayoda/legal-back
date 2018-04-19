165ef2948eaccf0575c57a09035b17d380f2a934
Volderpayoda <volderpayoda@gmail.com>
2018-04-19 13:43:49 -0300

    correccion fecha

--- public/js/alta-ordenanzas-ctrl.js
+++ public/js/alta-ordenanzas-ctrl.js
@@ -19,12 +19,12 @@ app.controller("alta-ordenanzas-ctrl", function($scope, $http, $state) {
             ordenanza.sub16, ordenanza.sub17, ordenanza.sub18,
             ordenanza.sub19, ordenanza.sub20, ordenanza.sub21,
             ordenanza.sub22, ordenanza.sub23, ordenanza.sub24,
-            ordenanza.sub25, ordenanza.sub26, ordenanza.sub27,]
+                ordenanza.sub25, ordenanza.sub26, ordenanza.sub27,]
         };
     
-    $http.post("http://volderpayoda.sytes.net/api/ordenanzas", JSON.stringify(ordenanza))
-        .then(function(ordenanza){
+    $http.post("http://volderpayoda.sytes.net/api/ordenanzas", JSON.stringify(data))
+        .then(function(data){
             $state.go('submit');    
-        }), function(ordenanza){
+        }), function(data){
         $scope.msg = "ERROR: No pudimos enviar tu ordenanza"}
 }});
