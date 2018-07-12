a23c2e52450ebb590fbc58603dbfd2bee74a8a47
Volderpayoda <volderpayoda@gmail.com>
2018-07-12 10:47:01 -0300

    Corrigiendo acceso a la URI de Postgre

--- db/index.js
+++ db/index.js
@@ -1,5 +1,5 @@
 const { Pool } = require('pg')
-const connectionString = DATABASE_URL;
+const connectionString = process.env.DATABASE_URL;
 
 const pool = new Pool({
   connectionString: connectionString
